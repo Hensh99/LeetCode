ee2b4d32b95b9ed6d0e2f38e957a0594aa52014f
Mohamed Hany <mohamedhany.mh.1999@gmail.com>
2022-07-27 05:36:14 +0300

    Time: 544 ms (31.20%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 SET N = N-1;
   RETURN (
-      SELECT DISTINCT(salary) from Employee ORDER BY salary DESC
+      SELECT DISTINCT(salary) FROM Employee ORDER BY salary DESC
       LIMIT 1 OFFSET N      
   );
 END
